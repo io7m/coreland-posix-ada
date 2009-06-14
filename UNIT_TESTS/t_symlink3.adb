97738a91cd2904daaaccf71c1eeaaeda308d55c8
m0 <m0@localhost>
2009-06-14 23:59:31 +0100

    Fix tests.

--- UNIT_TESTS/t_symlink3.adb
+++ UNIT_TESTS/t_symlink3.adb
@@ -8,7 +8,7 @@ with Test;
 procedure T_Symlink3 is
   Error_Value : POSIX.Error.Error_t;
   Name_Buffer : POSIX.File.File_Name_t;
-  Name_Size   : POSIX.File.File_Name_Index_t;
+  Name_Size   : POSIX.File.File_Name_Size_t;
 begin
   POSIX.File.Unlink ("tmp/symlink3", Error_Value);
   Test.Assert (Error_Value = POSIX.Error.Error_No_Such_File_Or_Directory);
