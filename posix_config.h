b8e066e63bf1ebb18ddfdcb375cca873ebd27d92
m0 <m0@localhost>
2009-06-17 00:29:38 +0100

    Add strict conformance defines.

--- posix_config.h
+++ posix_config.h
@@ -2,12 +2,16 @@
 #define POSIX_CONFIG_H
 
 /*
+ * Configuration for a strictly conforming XSI/POSIX program.
+ *
  * The following definitions are required to provide standardized
  * versions of functions as opposed to proprietary GNU extensions.
  */
 
-#define _POSIX_C_SOURCE 200112L
-#define _XOPEN_SOURCE   600
+#define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE   700
+#define __BSD_VISIBLE   1
+#define __USE_XOPEN
 #undef  _GNU_SOURCE
 
 #endif
