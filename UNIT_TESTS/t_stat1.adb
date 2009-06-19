d4429941cc38e904ce5ae02c434080bdedf1816a
m0 <m0@localhost>
2009-06-19 01:09:23 +0100

    Fix test.

--- UNIT_TESTS/t_stat1.adb
+++ UNIT_TESTS/t_stat1.adb
@@ -18,6 +18,6 @@ begin
   Test.Assert (Error_Value = POSIX.Error.Error_None);
   Test.Assert (POSIX.File_Status.Is_Valid (Status));
 
-  Test.Assert (POSIX.File_Status.Get_Size (Status) = 4);
+  Test.Assert (POSIX.File_Status.Get_Size (Status) = 5);
   Test.Assert (POSIX.File_Status.Get_Number_Of_Links (Status) = 1);
 end T_Stat1;
