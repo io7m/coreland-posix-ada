471b93b06c7b63f32c9f1547514fe8040704cf0f
m0 <m0@localhost>
2009-06-19 01:01:25 +0100

    Add stat test.

---
+++ UNIT_TESTS/t_stat1.adb
@@ -0,0 +1,20 @@
+with POSIX.Error;
+use type POSIX.Error.Error_t;
+
+with POSIX.File_Status;
+with Test;
+
+procedure T_Stat1 is
+  Error_Value : POSIX.Error.Error_t;
+  Status      : POSIX.File_Status.Status_t;
+begin
+  POSIX.File_Status.Get_Status
+    (File_Name   => "tmp/stat1",
+     Status      => Status,
+     Error_Value => Error_Value);
+  Test.Assert (Error_Value = POSIX.Error.Error_None);
+  Test.Assert (POSIX.File_Status.Is_Valid (Status));
+
+  Test.Assert (POSIX.File_Status.Get_Size (Status) = 4);
+  Test.Assert (POSIX.File_Status.Get_Number_Of_Links (Status) = 1);
+end T_Stat1;
