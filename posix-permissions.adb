933f0e06332ef60647957db1dfc4295b95cc4446
m0 <m0@localhost>
2009-06-19 15:13:50 +0100

    Add invariant for loop, for complete proof.

--- posix-permissions.adb
+++ posix-permissions.adb
@@ -4,11 +4,13 @@ package body POSIX.Permissions is
     Return_Mode : Mode_Integer_t := 8#0000#;
   begin
     for Mode_Element in Mode_Element_t range Mode_t'First .. Mode_t'Last loop
+      --# assert Mode_Element <= Mode_t'Last
+      --#    and Mode_Element >= Mode_t'First
+      --#    and Mode_Map (Mode_Element) >= Mode_Integer_t'First
+      --#    and Mode_Map (Mode_Element) <= Mode_Integer_t'Last;
       if Mode (Mode_Element) then
         Return_Mode := Return_Mode or Mode_Map (Mode_Element);
       end if;
-      --# assert (Mode_Element <= Mode_t'Last) and
-      --#        (Mode_Element >= Mode_t'First);
     end loop;
     return Return_Mode;
   end Mode_To_Integer;
