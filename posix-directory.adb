6bccd2170d46b3249441c049d22673b59af4cd00
m0 <m0@localhost>
2009-06-27 02:36:43 +0100

    Directory ops.

--- posix-directory.adb
+++ posix-directory.adb
@@ -1,4 +1,11 @@
+with C_String;
+with Interfaces.C;
+
 package body POSIX.Directory is
+
+  --
+  -- Change_By_Descriptor.
+  --
 
   procedure Change_By_Descriptor
     (Descriptor  : in     File.Valid_Descriptor_t;
@@ -14,6 +21,10 @@ package body POSIX.Directory is
       Error_Value := Error.Error_None;
     end if;
   end Change_By_Descriptor;
+
+  --
+  -- Change_By_Name.
+  --
 
   procedure Change_By_Name
     (Path        : in     String;
@@ -41,5 +52,69 @@ package body POSIX.Directory is
       Error_Value := Error_Local;
     end if;
   end Change_By_Name;
+
+  --
+  -- Create directory.
+  --
+
+  function Create_Boundary
+    (Name : in String;
+     Mode : in Permissions.Mode_t) return Error.Return_Value_t
+    --# global in Errno.Errno_Value;
+    --# return R =>
+    --#  ((R = 0)  -> (Error.Get_Error (Errno.Errno_Value)  = Error.Error_None)) or
+    --#  ((R = -1) -> (Error.Get_Error (Errno.Errno_Value) /= Error.Error_None));
+  is
+    C_Name : aliased Interfaces.C.char_array := Interfaces.C.To_C (Name);
+
+    function Mkdir
+      (Name : in C_String.String_Not_Null_Ptr_t;
+       Mode : in Permissions.Mode_t) return Error.Return_Value_t;
+    pragma Import (C, Mkdir, "mkdir");
+  begin
+    return Mkdir
+      (Name => C_String.To_C_String (C_Name'Unchecked_Access),
+       Mode => Mode);
+  end Create_Boundary;
+
+  procedure Create
+    (Name        : in     String;
+     Mode        : in     Permissions.Mode_t;
+     Error_Value :    out Error.Error_t) is
+  begin
+    case Create_Boundary (Name, Mode) is
+      when  0 => Error_Value := Error.Get_Error;
+      when -1 => Error_Value := Error.Error_None;
+    end case;
+  end Create;
+
+  --
+  -- Remove empty directory.
+  --
+
+  function Remove_Boundary (Name : in String) return Error.Return_Value_t
+    --# global in Errno.Errno_Value;
+    --# return R =>
+    --#  ((R = 0)  -> (Error.Get_Error (Errno.Errno_Value)  = Error.Error_None)) or
+    --#  ((R = -1) -> (Error.Get_Error (Errno.Errno_Value) /= Error.Error_None));
+  is
+    C_Name : aliased Interfaces.C.char_array := Interfaces.C.To_C (Name);
+
+    function Rmdir (Name : in C_String.String_Not_Null_Ptr_t)
+      return Error.Return_Value_t;
+    pragma Import (C, Rmdir, "rmdir");
+  begin
+    return Rmdir (C_String.To_C_String (C_Name'Unchecked_Access));
+  end Remove_Boundary;
+
+  procedure Remove
+    (Name        : in     String;
+     Error_Value :    out Error.Error_t) is
+  begin
+    case Remove_Boundary (Name) is
+      when  0 => Error_Value := Error.Get_Error;
+      when -1 => Error_Value := Error.Error_None;
+    end case;
+  end Remove;
 
 end POSIX.Directory;
--- posix-directory.ads.sh
+++ posix-directory.ads.sh
@@ -5,6 +5,7 @@ cat auto-warn.txt || exit 1
 cat <<EOF
 with POSIX.File;
 with POSIX.Error;
+with POSIX.Permissions;
 
 use type POSIX.Error.Error_t;
 use type POSIX.Error.Return_Value_t;
@@ -12,7 +13,8 @@ use type POSIX.Error.Return_Value_t;
 --# inherit POSIX.C_Types,
 --#         POSIX.Error,
 --#         POSIX.Errno,
---#         POSIX.File;
+--#         POSIX.File,
+--#         POSIX.Permissions;
 
 package POSIX.Directory is
 
@@ -29,6 +31,21 @@ package POSIX.Directory is
      Error_Value :    out Error.Error_t);
   --# global in Errno.Errno_Value;
   --# derives Error_Value from Descriptor, Errno.Errno_Value;
+
+  -- subprogram_map : mkdir
+  procedure Create
+    (Name        : in     String;
+     Mode        : in     Permissions.Mode_t;
+     Error_Value :    out Error.Error_t);
+  --# global in Errno.Errno_Value;
+  --# derives Error_Value from Name, Mode, Errno.Errno_Value;
+
+  -- subprogram_map : rmdir
+  procedure Remove
+    (Name        : in     String;
+     Error_Value :    out Error.Error_t);
+  --# global in Errno.Errno_Value;
+  --# derives Error_Value from Name, Errno.Errno_Value;
 
 end POSIX.Directory;
 EOF
