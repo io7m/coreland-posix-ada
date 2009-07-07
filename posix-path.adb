44ab60c47fa10bc26a242844874c3f960569a47d
m0 <m0@viper.internal.network>
2009-07-07 14:12:02 +0100

    Add POSIX.Path body.

---
+++ posix-path.adb
@@ -0,0 +1,10 @@
+package body POSIX.Path is
+
+  procedure Remove_Component
+    (Path       : in     String;
+     Last_Index :    out Positive) is
+  begin
+    null;
+  end Remove_Component;
+
+end POSIX.Path;
--- posix-path.ads.sh
+++ posix-path.ads.sh
@@ -4,10 +4,14 @@ cat LICENSE       || exit 1
 cat auto-warn.txt || exit 1
 cat <<EOF
 package POSIX.Path is
-  pragma Pure (POSIX.Path);
 
   -- PATH_MAX
   Max_Length : constant Positive := `./constants PATH_MAX`;
+
+  -- Remove component of Path.
+  procedure Remove_Component
+    (Path       : in     String;
+     Last_Index :    out Positive);
 
 end POSIX.Path;
 EOF
