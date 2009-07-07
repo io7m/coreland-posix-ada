f862e6aca24604f7d47676dddbcdb00e6246cba8
m0 <m0@localhost>
2009-07-07 16:34:08 +0100

    Update path types.

--- posix-path.ads.sh
+++ posix-path.ads.sh
@@ -9,12 +9,12 @@ package POSIX.Path is
   Max_Length : constant Positive := `./constants PATH_MAX`;
 
   -- Path name subtype based on POSIX PATH_MAX
-  subtype Path_Name_Size_t  is Natural range Natural'First .. Path.Max_Length;
-  subtype Path_Name_Index_t is Path_Name_Size_t range Path_Name_Size_t'First + 1 .. Path.Max_Length;
+  subtype Path_Name_Size_t  is Natural range Natural'First .. Max_Length;
+  subtype Path_Name_Index_t is Path_Name_Size_t range Path_Name_Size_t'First + 1 .. Max_Length;
   subtype Path_Name_t       is String (Path_Name_Index_t);
 
   -- Remove component of Path.
-  function Remove_Component (Path_Name  : in String) return Positive;
+  function Remove_Component (Path_Name : in String) return Positive;
   --# pre Path_Name'First >= 1
   --# and Path_Name'Last  >= Path_Name'First;
 
